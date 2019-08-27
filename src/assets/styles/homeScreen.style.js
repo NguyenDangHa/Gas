3b22abac5c9100d4f21c30ff8a97321b7225b3b3
NguyenDangHa <hanguyendang@vooc.vn>
2019-08-27 09:26:09 +0700

    tev

---
+++ src/assets/styles/homeScreen.style.js
@@ -0,0 +1,30 @@
+import {
+    StyleSheet
+  } from 'react-native';
+const styles = StyleSheet.create({
+    container:{
+      flex: 1, 
+      backgroundColor: 'rgb(255,130,0)',
+      justifyContent:'center'
+  
+    },
+    Slideshow: {
+      width: 414,
+      height: 250,
+      alignItems: 'center',
+      marginBottom: 15,
+      marginTop: 15,
+    },
+    BottomView: {
+      borderWidth: 1,
+      borderColor: 'rgb(211,23,61)',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      height: 70,
+      width: '100%',
+      flexDirection: 'row',
+      backgroundColor: 'rgb(255,255,255)',
+      marginTop:203
+    }
+  });
+  
